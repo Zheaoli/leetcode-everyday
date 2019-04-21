a3cf84f518a310c7d6bdf57537abde97affaf2e9
Manjusaka <me@manjusaka.me>
2019-04-21 20:52:47 +0800

    lc 213

---
+++ 213.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) > 2:
+            return max([
+                self.self_rob(nums, 0,
+                              len(nums) - 1),
+                self.self_rob(nums, 1, len(nums))
+            ])
+        else:
+            return max(nums)
+
+    def self_rob(self, nums, start, end):
+        money = 0
+        money1 = 0
+        for i in range(start, end):
+            temp = money
+            money = max(money, money1)
+            money1 = temp + nums[i]
+        return max(money, money1)
