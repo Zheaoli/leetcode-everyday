857ff386f1963450480a3f242653bd57fc45d264
Manjusaka <me@manjusaka.me>
2019-07-23 23:52:46 +0800

    complete leetcode:0022.py

---
+++ 0022.py
@@ -0,0 +1,17 @@
+from typing import *
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        results = []
+        self.dfs(results, "", n, 0, 0)
+        return results
+
+    def dfs(self, results: List[str], temp: str, n: int, left: int, right: int) -> None:
+        if right == n:
+            results.append(temp)
+            return
+        if left < n:
+            self.dfs(results, temp + "(", n, left + 1, right)
+        if right < left:
+            self.dfs(results, temp + ")", n, left, right + 1)
