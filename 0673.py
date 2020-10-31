7135a5dac22968332d693338900748896b473edd
Manjusaka <me@manjusaka.me>
2020-10-31 17:59:40 +0800

    leetcode 0673.py

---
+++ 0673.py
@@ -0,0 +1,17 @@
+import collections
+import bisect
+
+
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        dp = collections.defaultdict(collections.Counter)
+        dp[-1][-1e9] = 1
+        table = []
+        for i in nums:
+            index = bisect.bisect_left(table, i)
+            if index == len(table):
+                table.append(i)
+            else:
+                table[index] = i
+            dp[index][i] += sum(dp[index - 1][j] for j in dp[index - 1] if j < i)
+        return sum(dp[max(0, len(table) - 1)].values())
