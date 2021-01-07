fe5a3af31bca18f86e75d83c5f8c76ffa908b100
Manjusaka <me@manjusaka.me>
2021-01-07 21:38:38 +0800

    leetcode 0547

---
+++ 0547.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def find(node):
+            if circles[node] == node: return node
+            root = find(circles[node])
+            circles[node] = root
+            return root
+
+        n = len(isConnected)
+        circles = {x: x for x in range(n)}
+        num = n
+        for i in range(n):
+            for j in range(i, n):
+                if i != j and isConnected[i][j] == 1 and find(i) != find(j):
+                    circles[find(i)] = find(j)
+
+        return sum([1 for k, v in circles.items() if k == v])
+
+
+print(Solution().findCircleNum([[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
