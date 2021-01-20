2ebd979a2b5674c3cc38e2ed1c5b0933a8535909
Manjusaka <me@manjusaka.me>
2021-01-20 22:27:28 +0800

    leetcode daily 0005

--- 0005.py
+++ 0005.py
@@ -15,7 +15,7 @@ class Solution:
         while left >= 0 and right < len(s) and s[left] == s[right]:
             left -= 1
             right += 1
-        return s[left + 1: right]
+        return s[left + 1 : right]
 
 
 Solution().longestPalindrome("babad")
