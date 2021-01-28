264a2157e6b705d0e922654b1ce02228066b1683
Manjusaka <me@manjusaka.me>
2021-01-29 00:02:23 +0800

    leetcode daily 1680

--- 1680.py
+++ 1680.py
@@ -1,6 +1,6 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
         final_result = []
-        for i in range(1,n+1):
+        for i in range(1, n + 1):
             final_result.append(bin(i)[2:])
         return int("".join(final_result), 2) % (10 ** 9 + 7)
